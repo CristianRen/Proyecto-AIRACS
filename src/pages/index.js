0d43f9a7056adc6a97f971257f6510631c34d0e2
Smith Yáñez <jhoan71002@gmail.com>
2023-05-30 01:37:51 -0500

    Paginas y patron de diseños

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+const Inicio = () => {
+    return (
+      <>
+      {/* Aquí va el contenido de la página de inicio */}
+      </>
+    );
+  };
+  
+export default Inicio;
+  
---
+++ src/pages/servicios/index.js
@@ -0,0 +1,9 @@
+const Servicios = () => {
+  return (
+    <>
+    {/* Aquí va el contenido de la página de nuestros servicios */}
+    </>
+  );
+};
+
+export default Servicios;
