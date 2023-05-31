f2d210491d21d587ad109a47d2136a4da6fa0de8
Smith Yáñez <jhoan71002@gmail.com>
2023-05-30 22:36:18 -0500

    imagenes en la pagina visualizacion

--- src/pages/visualizacion/index.js
+++ src/pages/visualizacion/index.js
@@ -1,9 +1,9 @@
 import "./globals.css";
-import Layout from "./layout";
+import LayoutPrincipal from "../../components/menu-principal/layout-principal";
 import usuarios from "./array-objetos";
 export default function UploadData() {
   return (
-    <Layout>
+    <LayoutPrincipal>
       <section className="bg-[#F4FFFE]">
         <div className="max-w-screen-xl px-4 py-8 mx-auto sm:px-6 sm:py-12 lg:px-8">
           <ul className="grid gap-4 mt-16 sm:grid-cols-2 lg:grid-cols-3">
@@ -13,20 +13,20 @@ export default function UploadData() {
           </ul>
         </div>
       </section>
-    </Layout>
+    </LayoutPrincipal>
   );
 }
 
 function Card(props) {
   return (
-    <li>
+    <li key={props.user.ID}>
       <a
         href="#"
         className="bg-white block rounded-lg p-4 shadow-lg shadow-indigo-100"
       >
         <img
           alt="Home"
-          src="https://images.unsplash.com/photo-1613545325278-f24b0cae1224?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+          src={props.user.IMAGENES}
           className="h-56 w-full rounded-md object-cover"
         />
 
